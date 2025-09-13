d8d2e90445cb8e83e396c5fd8334061722088929
Chengrun Jin <chengrunjin@Chengruns-MacBook-Air.local>
2025-09-13 17:45:25 -0400

    initial commit

--- src/scenes/TitleScreen.js
+++ src/scenes/TitleScreen.js
@@ -10,7 +10,12 @@ export default class TitleScreen extends Phaser.Scene {
     }
 
     create() {
-        this.add.text(400, 300, 'hewwo')
-        text.setOrigin(0.1, 0.5)
+        // Create the text object and assign it to a variable
+        const titleText = this.add.text(400, 300, 'hewwo', {
+            fontSize: '64px', // Made the text bigger for a title screen
+            color: '#ffffff'
+        });
+        // Now, you can set the origin of the text object
+        titleText.setOrigin(0.5, 0.5); // Centered the text
     }
 }
